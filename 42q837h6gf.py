e72aece37140bdf03a4c64f3e1fd5bca15760012
igamegod <155389885+igamegod@users.noreply.github.com>
2025-05-20 21:06:53 -0700

    Update 42q837h6gf.py

--- 42q837h6gf.py
+++ 42q837h6gf.py
@@ -5,17 +5,24 @@ from urllib.parse import urlparse, parse_qs
 JSON_FILE_PATH = "moddeditems.json"
 
 def response(flow: http.HTTPFlow):
+    if "/fortnite/api/v2/versioncheck/IOS" in flow.request.url:
+        flow.response = http.Response.make(
+            200,
+            json.dumps({"type": "NO_UPDATE"}),
+            {"Content-Type": "application/json"}
+        )
+        return
+
     target_url = "https://fngw-mcp-gc-livefn.ol.epicgames.com/fortnite/api/game/v2/profile/"
-    
     if target_url in flow.request.url:
         parsed_url = urlparse(flow.request.url)
         query_params = parse_qs(parsed_url.query)
-        
+
         if query_params.get("profileId") == ["athena"]:
             try:
                 with open(JSON_FILE_PATH, "r", encoding="utf-8") as f:
                     custom_response = json.load(f)
-                
+
                 flow.response.text = json.dumps(custom_response)
                 flow.response.headers["Content-Type"] = "application/json"
             except Exception:
